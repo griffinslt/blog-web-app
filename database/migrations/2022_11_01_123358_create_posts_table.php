9a459478e9791c842a621dfb8aa2080522b47b33
griffinslt <slt.griffin3105@gmail.com>
2022-11-01 13:31:51 +0000

    PostSeeder working

--- database/migrations/2022_11_01_123358_create_posts_table.php
+++ database/migrations/2022_11_01_123358_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
             $table->timestamps();
 
             $table->unsignedBigInteger('user_id');
