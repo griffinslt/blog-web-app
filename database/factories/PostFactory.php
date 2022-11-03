0aa798b105d0a0f6a7bf8d463095c43e290e930b
griffinslt <slt.griffin3105@gmail.com>
2022-11-03 13:36:57 +0000

    faker data changed for title of post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,7 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'title' => fake()->title(),
+            'title' => fake()->word(),
             'body' => fake()->text(),
             'user_id' => fake()->numberBetween(1,2),
         ];
