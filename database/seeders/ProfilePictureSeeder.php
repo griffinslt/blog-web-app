1e73a20072d21a6efaa3c49dec807c43224697ec
griffinslt <slt.griffin3105@gmail.com>
2022-11-04 10:05:05 +0000

    ProfilePictureSeeder Fixed

--- database/seeders/ProfilePictureSeeder.php
+++ database/seeders/ProfilePictureSeeder.php
@@ -15,17 +15,16 @@ class ProfilePictureSeeder extends Seeder
      */
     public function run()
     {
-        // $p = new ProfilePicture;
-        // $p->file_path = 'Users/sam/fakefile.png';
-        // $p->user_id = 1;
-        // $p->save();
+        $p = new ProfilePicture;
+        $p->file_path = 'Users/sam/fakefile.png';
+        $p->user_id = 1;
+        $p->save();
 
-        // $p2 = new ProfilePicture;
-        // $p2->file_path = 'Users/sam/fakefile.png';
-        // $p2->user_id = 2;
-        // $p2->save();
+        $p2 = new ProfilePicture;
+        $p2->file_path = 'Users/sam/fakefile.png';
+        $p2->user_id = 2;
+        $p2->save();
 
-        //ProfilePicture::factory()->count(2)->create();
 
     }
 }
