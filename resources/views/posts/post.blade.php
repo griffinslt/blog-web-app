1966a88694d01603f742c5aeb5be2553cada41ee
griffinslt <slt.griffin3105@gmail.com>
2022-12-22 16:12:13 +0000

    Updated comment section with box

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -19,16 +19,28 @@
 <hr/>
 
 
-   <h3> Comments </h2>
-    <div class= "p-3 border bg-light">
+<h3> Comments </h2>
+<div class= "p-3 border bg-light">
 @foreach ($comments as $comment)
     @if ($comment->post_id == $post->id)
+        @foreach ($users as $user)
+            @if ($comment->user_id == $user->id)
+                {{ $user->name }}
+                <p></p>
+            @endif
+        @endforeach
         {{ $comment->body }}
         <hr/>
         
     @endif
 @endforeach
 </div>
+<div class="form-outline">
+    <textarea class="form-control" id="textAreaExample" rows="4"></textarea>
+    <label class="form-label" for="textAreaExample"></label>
+  </div>
+<a class="btn btn-outline-primary" href="">Write Comment</a>
+<a href="/posts" class="btn btn-primary ">Go Back</a>
  
 
 
