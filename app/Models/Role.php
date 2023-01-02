811d0525b343f7b1c7a6a6b7b39ec1adb20b0165
griffinslt <slt.griffin3105@gmail.com>
2023-01-02 11:27:14 +0000

    Defined Role-User relationship and made migration

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Roles extends Model
+class Role extends Model
 {
     use HasFactory;
+
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     public function comments(){
         return $this->hasMany(Comment::class);
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
